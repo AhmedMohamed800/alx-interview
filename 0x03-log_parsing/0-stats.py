4a2439707730035598342a5ad75861980fa5b0cf
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-23 23:10:22 +0200

    Add 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,26 +3,28 @@
 import sys
 
 
-def print_result(total_size, status_count):
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_count):
-        print(f"{status_code}: {status_count[status_code]}")
-
-
-try:
+if __name__ == '__main__':
     line_count = 0
     total_size = 0
     status_count = {}
-    for line in sys.stdin:
-        parts = line.split()
-        line_count += 1
-        total_size += int(parts[8])
-        if parts[7] in status_count:
-            status_count[parts[7]] += 1
-        else:
-            status_count[parts[7]] = 1
-        if line_count % 10 == 0:
-            print_result(total_size, status_count)
-except KeyboardInterrupt:
-    print_result(total_size, status_count)
-    raise
+
+    def print_result(total_size, status_count):
+        print("File size: {:d}".format(total_size))
+        for status_code in sorted(status_count):
+            print(f"{status_code}: {status_count[status_code]}")
+
+    try:
+        for line in sys.stdin:
+            parts = line.split()
+            line_count += 1
+            total_size += int(parts[8])
+            if parts[7] in status_count:
+                status_count[parts[7]] += 1
+            else:
+                status_count[parts[7]] = 1
+            if line_count % 10 == 0:
+                print_result(total_size, status_count)
+        print_result(total_size, status_count)
+    except KeyboardInterrupt:
+        print_result(total_size, status_count)
+        raise
