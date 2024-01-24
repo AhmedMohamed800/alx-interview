468274f85227ab71b7dcabb8b201f8687fd34b17
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-01-24 10:14:41 +0200

    Add 0-sts

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python3
-"""0-states"""
-import sys
+"""0-stats.py
+
+Read logs from a generator
+"""
 
 
-if __name__ == '__main__':
-    line_count = 0
-    total_size = 0
-    code = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    status_count = {c: 0 for c in code}
+def print_stats(size, status_codes):
+    """Print stats.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
-    def print_result(total_size, status_count):
-        print("File size: {:d}".format(total_size))
-        for key, value in sorted(status_count.items()):
-            print("{}: {}".format(key, value))
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
 
     try:
         for line in sys.stdin:
-            parts = line.split()
-            line_count += 1
-            total_size += int(parts[8])
-            if parts[7] in status_count:
-                status_count[parts[7]] += 1
-            if line_count % 10 == 0:
-                print_result(total_size, status_count)
-        print_result(total_size, status_count)
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
     except KeyboardInterrupt:
-        print_result(total_size, status_count)
+        print_stats(size, status_codes)
         raise
