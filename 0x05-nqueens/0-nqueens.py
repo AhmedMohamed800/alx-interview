c8b97067d5369ff5bccd7dd0fc02172d78841172
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-05 14:45:54 +0200

    Add 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-The N queens puzzle is the challenge of placing N non-attacking queens on an NxN chessboard.
+The N queens puzzle is the challenge of placing
+N non-attacking queens on an NxN chessboard.
 """
 import sys
 
@@ -17,3 +18,59 @@ except ValueError:
     print("N must be a number")
     exit(1)
 
+
+def is_valid_state(state, n):
+    """check if the state is valid"""
+    # check state
+    return len(state) == n
+
+
+def get_candidates(state, n):
+    """generate candidates for solving"""
+    if not state:
+        return range(n)
+
+    # find the next position in the state
+    position = len(state)
+    candidates = set(range(n))
+
+    # prune down candidates that place the queen
+    for row, col in enumerate(state):
+        candidates.discard(col)
+        dist = position - row
+
+        candidates.discard(col+dist)
+        candidates.discard(col-dist)
+    return candidates
+
+
+def search(state, solutions, n):
+    """search through candidates"""
+    if is_valid_state(state, n):
+        convert_to_2d_array(state)
+        solutions.append(state.copy())
+        return
+
+    for candidate in get_candidates(state, n):
+        state.append(candidate)
+        search(state, solutions, n)
+        state.pop()
+
+
+def convert_to_2d_array(state):
+    """convert an array to a 2d array"""
+    for i in range(len(state)):
+        temp = state[i]
+        state[i] = [i, temp]
+
+
+def solveNQueen(n):
+    """solve the question"""
+    solutions = []
+    state = []
+    search(state, solutions, n)
+    return solutions
+
+
+for solution in solveNQueen(n):
+    print(solution)
