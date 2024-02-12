46fa6d80239013b7569314800e4d2c2e45874de3
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-12 14:36:07 +0200

    fix: bug in 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,15 +8,19 @@ request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, (error, _, body
     } else {
         const film = JSON.parse(body);
         const characters = film.characters;
-        characters.forEach((characterUrl) => {
-            request(characterUrl, (error, _, body) => {
-                if (error) {
-                    console.error('Error:', error);
-                } else {
-                    const character = JSON.parse(body);
-                    console.log(character.name);
-                }
-            });
-        });
+
+        const charactersName = characters.map(
+            url => new Promise((resolve, reject) => {
+                request(url, (promiseErr, __, charactersReqBody) => {
+                    if (promiseErr) {
+                        reject(promiseErr);
+                    }
+                    resolve(JSON.parse(charactersReqBody).name);
+                });
+            }));
+
+        Promise.all(charactersName)
+            .then(charactersNames => console.log(charactersNames.join('\n')))
+            .catch(errors => console.log(errors));
     }
 });
