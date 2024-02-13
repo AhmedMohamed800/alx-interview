ab5269c5e464107e39dc6a03d6879fd314f0c837
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-13 05:34:14 +0200

    fix lint

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,24 +3,24 @@
 const request = require('request');
 const movieId = process.argv[2];
 request(`https://swapi-api.alx-tools.com/api/films/${movieId}/`, (error, _, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else {
-        const film = JSON.parse(body);
-        const characters = film.characters;
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-        const charactersName = characters.map(
-            url => new Promise((resolve, reject) => {
-                request(url, (promiseErr, __, charactersReqBody) => {
-                    if (promiseErr) {
-                        reject(promiseErr);
-                    }
-                    resolve(JSON.parse(charactersReqBody).name);
-                });
-            }));
+    const charactersName = characters.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-        Promise.all(charactersName)
-            .then(charactersNames => console.log(charactersNames.join('\n')))
-            .catch(errors => console.log(errors));
-    }
+    Promise.all(charactersName)
+      .then(charactersNames => console.log(charactersNames.join('\n')))
+      .catch(errors => console.log(errors));
+  }
 });
