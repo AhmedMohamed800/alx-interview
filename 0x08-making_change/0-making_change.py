d8273f7586c772e119302ca17c8b514b55dc61e3
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-27 17:25:50 +0200

    edit 0-making

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,6 +6,8 @@ def makeChange(coins, total):
     """
     return the fewest number of coins needed to meet a given amount total.
     """
+    if total <= 0:
+        return 0
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
