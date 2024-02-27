2d32457a45d1a4bab81fe1a841190ef8b5b0d32d
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-27 17:33:39 +0200

    Add answer

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,8 +11,6 @@ def makeChange(coins, total):
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    coins.sort(reverse=True)
-    
     for i in range(1, total + 1):
         for j in range(0, len(coins)):
             if coins[j] <= i:
