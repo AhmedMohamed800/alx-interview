be6a4a67d3c06da0b219a7f68bfb9e6dc26457ec
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-02-27 17:20:07 +0200

    Add makeChange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,30 +2,15 @@
 """making_change module"""
 
 
-def min_great_than_zero(coins, total):
-    """"""
-    min = total - coins[0]
-    for coin in coins:
-        coin = total - coin
-        if min > coin and coin > 0:
-            min = coin
-    return min
-
-
-def search(steps, coins, total):
-    """search"""
-    if total < 0:
-        return -1
-    if total == 0:
-        return steps
-
-    shortest = min_great_than_zero(coins, total)
-    steps += 1
-    return search(steps, coins, shortest)
-
-
 def makeChange(coins, total):
     """
     return the fewest number of coins needed to meet a given amount total.
-    """    
-    return search(0, coins, total)
+    """
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for j in range(0, len(coins)):
+            if coins[j] <= i:
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+    return -1 if dp[total] > total else dp[total]
