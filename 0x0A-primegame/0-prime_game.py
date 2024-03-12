ed57900d50b13da1ce67e73174607cc3d1f92e65
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2024-03-12 11:54:05 +0200

    fix pyecode

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """0-prime_game.py"""
 
+
 def get_primes(n):
     """Return list of prime numbers between 1 and n inclusive"""
     primes = []
@@ -17,7 +18,7 @@ def isWinner(x, nums):
     """DET if Ben will win the game or Maria"""
     if x is None or nums is None or x == 0 or nums == []:
         return None
-    
+
     Maria = 0
     Ben = 0
 
@@ -27,7 +28,7 @@ def isWinner(x, nums):
             Ben += 1
         else:
             Maria += 1
-    
+
     if Maria > Ben:
         return 'Maria'
     elif Ben > Maria:
